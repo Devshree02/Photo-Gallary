4386ed703fb202d6bd6ab42d6c80968feab256bc
Devshree02 <devshreenighojakar0205@gmail.com>
2021-09-07 19:07:32 +0530

    search keyword

--- assests/js/custom.js
+++ assests/js/custom.js
@@ -22,6 +22,8 @@ document.addEventListener('DOMContentLoaded', function () {
     searchIc.style.display = "none";
 
     let inputId = document.getElementById("searchInput").value;
+    let inputSubString = inputId.slice(0,3);
+
     let allImg = document.getElementsByTagName("img");
     let lenImg = allImg.length;
 
@@ -54,7 +56,7 @@ document.addEventListener('DOMContentLoaded', function () {
     for (let i=0; i<lenImg; i++) {
       let imgAtt = allImg[i].getAttribute("data-image");
       // console.log(imgAtt);
-      if (imgAtt == inputId) {
+      if (imgAtt.includes(inputSubString)) {
         let image = document.createElement("img");
         image.src = allImg[i].src;
         document.getElementById("searchResult").appendChild(image);
