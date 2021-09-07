d1d1a207290188b081ba5623fd3e4613dd17e140
Devshree02 <devshreenighojakar0205@gmail.com>
2021-09-07 11:04:36 +0530

    Photo-Gallry Assignment

--- assests/js/custom.js
+++ assests/js/custom.js
@@ -1,5 +1,6 @@
 document.addEventListener('DOMContentLoaded', function () {
   document.getElementById("crossIcon").addEventListener("click", function() {
+    location.reload(); 
     document.getElementById("searchInput").value = "";
     let searchIc = document.getElementById("searchIcon");
     searchIc.style.display = "inline-block";
@@ -10,27 +11,55 @@ document.addEventListener('DOMContentLoaded', function () {
     // let dataId = getId.getAttribute('data-image');
   }); 
   document.getElementById("searchIcon").addEventListener("click" , function() {
+
+    let imgContainer = document.getElementById("imageContainer");
+    imgContainer.style.display = "none";
+
     let crossIc = document.getElementById("crossIcon");
     crossIc.style.display = "inline-block";
+
     let searchIc = document.getElementById("searchIcon");
     searchIc.style.display = "none";
+
     let inputId = document.getElementById("searchInput").value;
     let allImg = document.getElementsByTagName("img");
     let lenImg = allImg.length;
-    // let imgContainer = document.getElementById("imageContainer");
-    // imgContainer.style.visibility = "hidden";
-    console.log(lenImg);
-    for (let i = 0; i < lenImg; i++) {
+
+    
+    // console.log(lenImg);
+    // for (let i = 0; i < lenImg; i++) {
+    //   let imgAtt = allImg[i].getAttribute("data-image");
+    //   console.log(imgAtt);
+    //   console.log(inputId);
+    //   if (imgAtt == inputId) {
+    //     allImg[i].style.display = "";
+    //   }
+    //   else {
+    //     allImg[i].style.display = "none";
+    //   }
+    // }
+    // alert(lenimg);
+
+    // for (let i=0; i< lenImg; i++) {
+    //   let imgAtt = allImg[i].getAttribute("data-image");
+    //   if (inputId == imgAtt){
+        
+    //     // let srcImg = allImg[i].src;
+    //     // console.log(srcImg);
+    //     document.getElementById("searchResult").appendChild(allImg[i]);
+    //     console.log(allImg[i]);
+    //   }
+    // }
+
+    for (let i=0; i<lenImg; i++) {
       let imgAtt = allImg[i].getAttribute("data-image");
-      console.log(imgAtt);
-      console.log(inputId);
+      // console.log(imgAtt);
       if (imgAtt == inputId) {
-        allImg[i].style.display = "";
-      }
-      else {
-        allImg[i].style.display = "none";
+        let image = document.createElement("img");
+        image.src = allImg[i].src;
+        document.getElementById("searchResult").appendChild(image);
       }
     }
-    // alert(lenimg);
+
   });
 }());
