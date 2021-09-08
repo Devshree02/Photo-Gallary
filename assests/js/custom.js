fedf6af1c0657dd43f2933ed60d6f7be0ded4219
Devshree02 <devshreenighojakar0205@gmail.com>
2021-09-08 19:46:00 +0530

    Added multiple keywords for every image

--- assests/js/custom.js
+++ assests/js/custom.js
@@ -1,14 +1,6 @@
 document.addEventListener('DOMContentLoaded', function () {
   document.getElementById("crossIcon").addEventListener("click", function() {
     location.reload(); 
-    document.getElementById("searchInput").value = "";
-    let searchIc = document.getElementById("searchIcon");
-    searchIc.style.display = "inline-block";
-    let crossIc = document.getElementById("crossIcon");
-    crossIc.style.display = "none";
-
-    // let getId = document.getElementById("birdFirst");
-    // let dataId = getId.getAttribute('data-image');
   }); 
   document.getElementById("searchIcon").addEventListener("click" , function() {
 
@@ -28,34 +20,10 @@ document.addEventListener('DOMContentLoaded', function () {
     let lenImg = allImg.length;
 
     
-    // console.log(lenImg);
-    // for (let i = 0; i < lenImg; i++) {
-    //   let imgAtt = allImg[i].getAttribute("data-image");
-    //   console.log(imgAtt);
-    //   console.log(inputId);
-    //   if (imgAtt == inputId) {
-    //     allImg[i].style.display = "";
-    //   }
-    //   else {
-    //     allImg[i].style.display = "none";
-    //   }
-    // }
-    // alert(lenimg);
-
-    // for (let i=0; i< lenImg; i++) {
-    //   let imgAtt = allImg[i].getAttribute("data-image");
-    //   if (inputId == imgAtt){
-        
-    //     // let srcImg = allImg[i].src;
-    //     // console.log(srcImg);
-    //     document.getElementById("searchResult").appendChild(allImg[i]);
-    //     console.log(allImg[i]);
-    //   }
-    // }
-
+    
     for (let i=0; i<lenImg; i++) {
       let imgAtt = allImg[i].getAttribute("data-image");
-      // console.log(imgAtt);
+      console.log(imgAtt);
       if (imgAtt.includes(inputSubString)) {
         let image = document.createElement("img");
         image.src = allImg[i].src;
